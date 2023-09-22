4c784a8f7e62d3ba258ad6f5d6a5f454d891410b
FrosT2k5 <androygaming@gmail.com>
2023-09-22 23:28:09 +0530

    main.py: Run telegram bot webhook and fastapi differently Running two http servers on same port is currently not possible to be done with the platform currently this bot is hosted in. So make separate instances of both which will run on different url independently

--- main.py
+++ main.py
@@ -3,49 +3,31 @@ import asyncio
 from sys import argv
 from os import environ
 
-async def startbot():
-    # import startserver function here to avoid error due to cyclical import
-    from server import startserver
-    print("Starting Bot")
-    await registerhandlers(botapplication)
+useWebhook = False
 
+try:
+    webhookarg = argv[1]
+    if webhookarg == "webhook":
+        useWebhook = True
+except IndexError:
     useWebhook = False
 
-    try:
-        webhookarg = argv[1]
-        if webhookarg == "webhook":
-            useWebhook = True
-    except IndexError:
-        useWebhook = False
+def startbotwebhook():
+    print("Starting telegram bot using webhook")
+    registerhandlers(botapplication)
+    botapplication.run_webhook(
+        listen = "0.0.0.0",
+        port = int(environ.get("PORT")),
+        secret_token = environ.get("WEBHOOK_SECRET_TOKEN"),
+        webhook_url = environ.get("WEBHOOK_URL")
+    )
 
-    #start the bot
-    async with botapplication as b:
-        if useWebhook:
-            print("Using webhook")
-            await b.initialize()
-            await b.start()
-            await b.updater.start_webhook(
-                listen = "0.0.0.0",
-                url_path="1",
-                port = int(environ.get("PORT")),
-                secret_token = "frost2k5",
-                webhook_url = "https://pingme.domcloud.io/1"
-            )
-            await startserver()
-            await b.updater.stop()
-            await b.stop()
-            await b.shutdown()
+async def startapiserver():
+    from server import startserver
+    await startserver()
 
-        else:
-            await b.initialize()
-            await b.start()
-            await b.updater.start_polling()
-            print("Starting server")
-            await startserver() # This function starts the fastapi server asynchronously
-            print("Reached EOL")
-            await b.updater.stop()
-            await b.stop()
-            await b.shutdown()
-
-if __name__=="__main__":
-    asyncio.run(startbot())
+if __name__ == "__main__":
+    if useWebhook:
+        startbotwebhook()
+    else:
+        asyncio.run(startapiserver())
