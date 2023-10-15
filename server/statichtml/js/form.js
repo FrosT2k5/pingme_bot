6c5f7df47bac169f13a50d0b4fab641f4dd156cc
QuantumByteStuduos <quantumbytestudios@gmail.com>
2023-10-15 18:45:07 +0530

    server: homepage: use bootstrap modals instead of alert Looks cleaner Additional mininal UI changes
    Signed-off-by: FrosT2k5 <androygaming@gmail.com>

--- server/statichtml/js/form.js
+++ server/statichtml/js/form.js
@@ -1,43 +1,55 @@
+function showModal(status) {
+  if (status == null) {
+    //
+  } else {
+    document.getElementById("status_box").innerHTML = status;
+    document.getElementById("status-button").click();
+  }
+}
+
 async function sendMessage() {
-    const hostName = window.location.origin;
-    let messageForm = document.forms['messageForm']
-    let username = messageForm['telegramUsername'].value;
-    let message = messageForm['message'].value;
-    let key = parseInt(messageForm['securityKey'].value);
+  const hostName = window.location.origin;
+  let messageForm = document.forms["messageForm"];
+  let username = messageForm["telegramUsername"].value;
+  let message = messageForm["message"].value;
+  let key = parseInt(messageForm["securityKey"].value);
 
-    document.getElementById('submit-button').disabled = true
-    setTimeout(() => document.getElementById('submit-button').disabled = false,4000)
+  document.getElementById("submit-button").disabled = true;
+  setTimeout(
+    () => (document.getElementById("submit-button").disabled = false),
+    4000
+  );
 
-    if (isNaN(key)) {
-        alert("Please enter a number in security key field.");
-        return 0;
-    }
+  if (isNaN(key)) {
+    alert("Please enter a number in security key field.");
+    return 0;
+  }
 
-    let reqJson = {
-        "username" : username,
-        "message" : message,
-        "securitykey" : key
-    }
-    console.log(reqJson)
+  let reqJson = {
+    username: username,
+    message: message,
+    securitykey: key,
+  };
+  console.log(reqJson);
 
-    let query = await fetch(hostName+'/api/sendmessage', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(reqJson)
-    } )
+  let query = await fetch(hostName + "/api/sendmessage", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(reqJson),
+  });
 
-    let respJson = await query.json();
-    console.log(respJson)
-    if (respJson.hasOwnProperty('error')) {
-        alert("Error: "+respJson['error']);
-        return 0;
-    }
-    if (respJson['status'] != 'success') {
-        alert("Unknown error occured. Please contact developer.");
-        return 0;
-    }
+  let respJson = await query.json();
+  console.log(respJson);
+  if (respJson.hasOwnProperty("error")) {
+    showModal("ERROR: " + respJson["error"]);
+    return 0;
+  }
+  if (respJson["status"] != "success") {
+    showModal("UNKNOWN ERROR");
+    return 0;
+  }
 
-    alert("Status: "+ respJson['status'])  
+  showModal("SUCCESS");
 }
