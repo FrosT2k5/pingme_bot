a34678fff3fc8c3a9589a4c085885abe7951d37c
FrosT2k5 <androygaming@gmail.com>
2024-03-09 18:37:01 +0530

    server: Add support for sending files /api/sendfile is used to send file to telegram, filesizes is hardcoded to be max 10MB

--- server/servermanager.py
+++ server/servermanager.py
@@ -1,11 +1,13 @@
-from fastapi import FastAPI,Response,status
+from fastapi import FastAPI,Response,status,UploadFile,File,Form
+from typing import Annotated
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from bot.sqlhandler import getusingusername
-from telegram.error import Forbidden
+from telegram.error import Forbidden,TimedOut
 import uvicorn
 from os import environ
+import bot
 
 app = FastAPI()
 app.add_middleware(
@@ -20,11 +22,11 @@ class message_model(BaseModel):
     username: str
     message: str
     securitykey: int
-
+    
 # Post method to send message, accepts json with username, message and securitykey
 @app.post("/api/sendmessage")
 async def sendmessage(messagedata: message_model,response: Response):
-    from bot import botapplication # TODO: remove this import and make a function in botapplication to handle this
+    botapplication = bot.botapplication # TODO: remove this import and make a function in botapplication to handle this
     messagedict = messagedata.dict()
     uid = getusingusername(messagedict['username'])
 
@@ -58,6 +60,42 @@ async def sendmessage(messagedata: message_model,response: Response):
 async def healthcheck():
     return {"status":"ok"}
 
+# class properties_model(BaseModel):
+#     username: str
+#     securitykey: int
+
+@app.post("/api/sendfile")
+async def send_file(file: Annotated[UploadFile, File()], username: Annotated[str, Form()], securitykey: Annotated[int, Form()],response: Response):
+    uid = getusingusername(username)
+
+    file_content = await file.read()
+    file_size = file.file.tell()
+
+    if (file_size/1024/1024) > 10:
+        response.status_code = status.HTTP_413_REQUEST_ENTITY_TOO_LARGE
+        return {"status": "error","error": "Filesize is larger than 10MB"}
+
+    if uid == 0:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"status":"error","error": "User not found in database, did you /start the bot?"}
+    realsecuritykey = uid[2]
+
+    if realsecuritykey != securitykey:
+        response.status_code = status.HTTP_401_UNAUTHORIZED
+        return {"status":"error","error": "Invalid Security key"}
+    
+    botapplication = bot.botapplication
+    try:
+        await botapplication.bot.send_document(uid[1], file_content, filename=file.filename, write_timeout=35)
+        return { "status": "success" }
+    except TimedOut:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return { "status": "error", "error": "Telegram timed out, try retrying."}
+    except Exception as e:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        error = f"Unknown error: {e}, please contact the maintainer of bot at Telegram @SuperCosmicBeing"
+        return {"status":"error","error": error} 
+           
 # mount the / to statichtml/ folder so the base url opens up the form
 app.mount("/", StaticFiles(directory="server/statichtml/",html=True),name="form")
 
